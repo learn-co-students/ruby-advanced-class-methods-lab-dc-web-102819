58829e538568dacf2911bf38177ff5c6249dad79
Flatiron School <stvik2@gmail.com>
2019-11-02 10:46:58 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.save
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}.uniq
+  end
+
+  def self.new_from_filename(filename)
+    delimiters = [" - ", "."]
+    filename_split = filename.split(Regexp.union(delimiters))
+    new_song = self.create_by_name(filename_split[1])
+    new_song.artist_name = filename_split[0]
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    @@all=[]
+  end
 end
